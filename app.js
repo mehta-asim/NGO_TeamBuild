bb252c6ba3aa5cbfa21a9304f9d01ad3cccd13a2
mehta-asim <72264877+mehta-asim@users.noreply.github.com>
2022-01-11 14:26:30 -0500

    fixed port issue

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 
 import { createServer } from "http";
 
-const hostname = "127.0.0.1";
-const port = 3000;
+// const hostname = "127.0.0.1";
+const port = process.env.PORT || 3000;
 
 const server = createServer((req, res) => {
   res.statusCode = 200;
@@ -11,6 +11,6 @@ const server = createServer((req, res) => {
   res.end("<h1>This is really easy!</h1>");
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+  console.log(`Server running at port: ${port}/`);
 });
